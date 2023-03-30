9d632f3fa43c1300cd9d4f2624e7a75b701a7a11
Anna Mitchell <annaeileenmitchell@gmail.com>
2023-03-30 15:03:53 -0500

    created db/cats

--- db/cats.js
+++ db/cats.js
@@ -1,1 +1,32 @@
 const {client} = require("./client");
+
+async function createCats({name, breed, age, temperment, outdoor, adoption, imageURL}) {
+    try {
+        const {rows} = await client.query(`
+        INSERT INTO activities(name, breed, age, temperment, outdoor, adoption, "imageURL")
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        ON CONFLICT (name) DO NOTHING
+        RETURNING *;
+        `,[name, breed, age, temperment, outdoor, adoption, imageURL]);
+
+        return rows;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getCats() {
+    try {
+        const {rows} = await client.query(`
+        SELECT * FROM cats;
+        `)
+
+        return rows;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+   createCats, getCats
+}
