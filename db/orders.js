acd56792de49139bb032ea687c3b6c46d97462da
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-04-11 13:45:52 -0400

    cleaned up orders db function

--- db/orders.js
+++ db/orders.js
@@ -1,7 +1,6 @@
 const {client} = require("./client");
 const { getPurchasesById } = require("./purchases");
-// const { getUserById } = require("./users");
-// push later on
+
 
 async function createOrders({
     userId, 
@@ -64,8 +63,6 @@ async function getOrdersById(id) {
         WHERE orders.id=$1;
         `,[id])
 
-        console.log("This is the order in the getOrdersById function")
-        console.log(order);
 
         if (!Object.keys(order).length) return undefined
 
@@ -75,8 +72,6 @@ async function getOrdersById(id) {
         WHERE purchases."orderId" = $1;
         `,[id])
 
-        console.log("This is the rows in that function")
-        console.log(rows);
         order.cats = rows;
         return order
     } catch (error) {
@@ -84,78 +79,26 @@ async function getOrdersById(id) {
     }
 }
 
-// async function getAllOrders() {
-//     try{
-//         const {rows: ids} = await client.queary(`
-//         SELECT id FROM orders;
-//         `)
 
-//         const orders = await Promise.all(ids.map(
-//             orders => getOrdersById(orders.id)
-//         ))
-//         return 
-//     } catch {
-//         console.log(error)
-//     }
-// }
-// what about getAllPendingOrders() 
 
 async function getAllOrdersByUser(userId) {
     try {
-        console.log("This is the userId in the getAllOrdersByUser functions")
-        console.log(userId);
-        
+
         
     const {rows} = await client.query(`
     SELECT * FROM orders
     WHERE "userId"=$1;
     `,[userId]);
 
-    console.log(rows);
 
-    // const orders = await Promise.all(ids.map(
-    //     orders => getOrdersById(orders.id)
-    // ))
     return rows;
     } catch (error) {
         console.log(error)
     }
 }
 
-// getAllPendingOrdersByUser
-//getAllPurchasesByUser in purchases
-
-//helper function:
-// async function attachActivitiesToRoutines(routines) {
-//     try {
-//         console.log(routines)
-//         const {rows} = await client.query(`
-//             SELECT * from activities
-//             JOIN "RoutineActivities"
-//             ON activities.id = "RoutineActivities"."activityId";
-//         `);
-//         console.log("This is routines in attachActivitiesToRoutines function");
-//         console.log(routines)
-//         for(let i=0; i<routines.length; i++){
-//             let answer = rows.filter((singleActivity)=>{
-//                 if(singleActivity.routineId == routines[i].id){
-//                     return true;
-//                 }else{
-//                     return false;
-//                 }
-//             })
-//             routines[i].activities = answer;
-//         }
-//         return routines;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
 
 
-// getAllPendigOrdersByCats
-
-// if we have an error - use talal's code 
 async function updateOrders ({id, fields = {}}){
     
     const setString = Object.keys(fields).map(
@@ -198,35 +141,22 @@ async function destroyOrders(id) {
         console.log(error)
     }
 }
-//submitOrder function to take in a userId and make their order status=submitted, 
-//creates a new order for them with status=pending
-//getuserbyId
-// async function addCreditCardInformation({id, fields = {}}){
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// }
 
 
-//finish an order => 
-    //update order and set status to submitted, 
-    //make a new order for that user
+
+
 
 //helper function
 async function getPendingOrderByUserId(userId){
     try {
-        console.log("this is the userId");
-        console.log(userId);
+
         const {rows} = await client.query(`
             SELECT * from orders
             WHERE "userId" = $1
             AND status='pending';
         `,[userId]);
         
-        console.log("This is the row in the async function");
-        console.log(rows);
+ 
 
        if(rows.length == 1)
        {
@@ -250,7 +180,7 @@ async function getPendingOrderByUserId(userId){
 //for profile page seeing all finished orders.
 async function getAllFinishedOrdersByUserId(userId){
     try {
-        console.log(userId);
+        
         const {rows} = await client.query(`
             SELECT * from orders
             WHERE "userId" = $1
@@ -324,7 +254,7 @@ async function getEntireCartByUserId(userId){
 async function finishOrder(userId){
 
     try {
-        // const user= await getUserById(userId);
+        
         const order = await getOrderByUserId(userId);
 
         const {rows} = await client.query(`
@@ -344,10 +274,9 @@ async function finishOrder(userId){
         }else{
             return "Failed to Submit Order";
         }
-        //
+        
         return order;
-        //update order ->pending to submitted
-        //make a new order for that user
+
 
     } catch (error) {
         console.log(error);
