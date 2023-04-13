018c5a7e83eb89226e58de256e733c01acf21c2c
Talal-mahmoudi <tkm16c@my.fsu.edu>
2023-04-13 13:41:07 -0400

    added render url

--- db/client.js
+++ db/client.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const { Client } = require('pg');
-// const connection = process.env.DATABASE_URL || 'postgres://localhost:5432/TheCatsPawjamas'
-const connection = 'postgres://localhost:5432/TheCatsPawjamas'
+const connection = process.env.DATABASE_URL || 'postgres://localhost:5432/TheCatsPawjamas'
+// const connection = 'postgres://localhost:5432/TheCatsPawjamas'
 const client = new Client(connection);
-// client.password = process.env.RENDER_PASSWORD;
+client.password = process.env.RENDER_PASSWORD;
 
 module.exports = {
     client
